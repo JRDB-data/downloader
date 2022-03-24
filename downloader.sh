1173439734e3c56bc7e52b8315e603386053a495
masaru.terada <mterada1228@gmail.com>
2022-03-25 08:46:47 +0900

    JRDB からデータをダウンロードするスクリプトを追加

---
+++ downloader.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+JRDB_BASE_URL=http://www.jrdb.com/member/data/
+EXTENTION=.lzh
+
+filepath() {
+  if [ ${FILETYPE} = 'Ks' ]; then
+    FILENAME_PREFIX='KZA'
+  else
+    FILENAME_PREFIX=$(tr '[a-z]' '[A-Z]' <<< $FILETYPE)
+  fi
+
+  echo ${FILETYPE}/${FILENAME_PREFIX}${FILEDATE}${EXTENTION}
+}
+
+file_exists() {
+  curl -u ${JRDB_USER}:${JRDB_PASSWORD} ${JRDB_BASE_URL}$(filepath) -o /dev/null -w '%{http_code}\n' -s
+}
+
+download() {
+  curl -u ${JRDB_USER}:${JRDB_PASSWORD} ${JRDB_BASE_URL}$(filepath) -L -o ${OUTPUT_DIRECTORY}$(filepath)
+}
+
+decompression() {
+  lha -xw=${OUTPUT_DIRECTORY}${FILETYPE} ${OUTPUT_DIRECTORY}$(filepath)
+}
+
+pre_process() {
+  mkdir -p ${OUTPUT_DIRECTORY}${FILETYPE}
+}
+
+post_process() {
+  rm -f ${OUTPUT_DIRECTORY}$(filepath)
+}
+
+main() {
+  pre_process
+
+  status_code=`file_exists`
+  if [ $status_code != 200 ]
+  then
+    echo ${JRDB_BASE_URL}$(filepath) is not found.
+    exit 0
+  fi
+
+  download
+  decompression
+  post_process
+}
+
+cat <<EOS
+
+Filetype? (ex. Ks)
+
+以下のファイルタイプが選択できます。
+===================================
+JRDB 騎手データ  : Ks
+JRDB 番組データ  : Bac
+JRDB 登録馬データ: Kta
+JRDB 馬基本データ: Ukc
+JRDB 競争馬データ: Kyi
+===================================
+EOS
+
+read FILETYPE
+
+cat <<EOS
+
+Filedate? (ex. 220319)
+yymmdd の形式で入力してください。
+EOS
+
+read FILEDATE
+
+main
